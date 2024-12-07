aef59e0c01971e5d12e91a70c4176e5f18dcedc5
lecaotanloc289 <lecaotanloc289@gmail.com>
2024-12-07 17:53:57 +0700

    config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SongsModule } from './songs/songs.module';
 import { AlbumModule } from './album/album.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configuration';
 import { validate } from './env.validation';
 
@@ -17,7 +17,14 @@ import { validate } from './env.validation';
       isGlobal: true,
       // validate: validate,
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/spotify'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/spotify'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     SongsModule,
     AlbumModule,
   ],
